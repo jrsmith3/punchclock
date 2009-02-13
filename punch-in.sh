eb7c3209f1749b3c92266a73955b24ca52f82884
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2009-02-13 03:03:15 +0000

    Added script to record kdm login time.

---
+++ punch-in.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script goes into the ~/.kde/Autostart directory. When you log into kde, every script in this directory gets executed.
+
+date > /home/jrsmith3/Desktop/punch-in.txt
