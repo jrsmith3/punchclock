787b2685825bc79db67b6cfda0bef7f891d63d09
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2009-02-26 01:19:56 +0000

    Added a license (GPL). Updated punch-in.py to only record the login time. Began work on updating punch-out.py to do the rest of the work.

--- punch-in.py
+++ punch-in.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python
-# This script goes into the ~/.kde/Autostart directory. When you log into kde, every script in this directory gets executed.
+"""Record datetime when user logs into KDE
 
-# Here's how this script should work
-# 1. Create an ical event with dtstamp, dtstart, summary = At work, 
-# and timezone info.
-# 2. Save this half of the event in ~/.kde/share/apps/punchclock with the
-# name punch-in.ics
-# 3. Add a line to a backup data file. These scripts wind up altering the 
-# punchclock.ics file in a way that might cause some corruption. I want to make 
-# sure that I don't catastrophically lose data one day by making a backup file.
+This program is part of the punchclock suite. It simply records the date and 
+time of a user's login to KDE and records that information in a simple text 
+file. The program punch-out.py does everything else.
+"""
+
+__author__ = "Joshua Ryan Smith (joshua.r.smith@gmail.com)"
+__version__ = ""
+__date__ = ""
+__copyright__ = "Copyright (c) 2009 Joshua Ryan Smith"
+__license__ = "GPL v3"
 
 import datetime
-import vobject
 from dateutil.tz import *
 
-timenow = datetime.datetime.now(tzlocal())
+timein = datetime.datetime.now(tzlocal()) + "\n"
 
-cal = vobject.iCalendar()
-cal.add('vevent')
-cal.vevent.add('dtstamp').value = timenow
-cal.vevent.add('dtstart').value = timenow
-cal.vevent.add('summary').value = "At work"
-
-# Open file to (over)write. 
-# !Must fix the hard-coded path.
-punchin = open('/home/jrsmith3/.kde/share/apps/punchclock/punchin.ics','w')
-punchin.write(cal.serialize())
+# Write current datetime to a temporary file using pickle.
+punchin = open('/home/jrsmith3/.kde/share/apps/punchclock/punchin.dat','w')
+punchin.write(timein)
 punchin.close()
-
-# Write some data to a simple text file that can be parsed at a later date:
-punchbak = open('/home/jrsmith3/.kde/share/apps/punchclock/punchclock.bak','a')
-punchbak.write("in,"+str(datetime.datetime.now())+"\n")
-punchbak.close()
--- punch-out.py
+++ punch-out.py
@@ -1,7 +1,67 @@
 #!/usr/bin/python
+"""Record when a user logs in and out of KDE as an iCalendar file.
+
+This program is part of the punchclock suite. Along with punch-in.py it 
+generates and iCalendar file with the users login and logout data as events. 
+This program takes the login data that the punch-in.py program generated along
+with the current datetime and turns this data into an iCanedar event. The 
+program then adds this event to an iCalendar file containing other such events,
+or creates the ics file if none exists. This program also records the login and
+logout data to a separate text file as a backup.
+"""
+
+__author__ = "Joshua Ryan Smith (joshua.r.smith@gmail.com)"
+__version__ = ""
+__date__ = ""
+__copyright__ = "Copyright (c) 2009 Joshua Ryan Smith"
+__license__ = "GPL v3"
+
 import vobject
 import datetime
 from dateutil.tz import *
+
+# Collect the datetime of logout.
+timeout = datetime.datetime.now(tzlocal())
+
+# Collect the datetime of the login event.
+#timein = 
+
+# Generate an iCalendar event.
+
+# Add to the existing ics file.
+
+# Record login/logout events in a separate text file for backup.
+punchclockbackupfile = open('/home/jrsmith3/.kde/share/apps/punchclock/punchclockbackup.txt','a')
+punchclockbackupfile.write("in,"+str(timein)+"\n")
+punchclockbackupfile.write("out,"+str(timeout)+"\n")
+punchclockbackupfile.close()
+
+
+# ========= vvv Old code from punch-in.py vvv =================================
+#cal = vobject.iCalendar()
+#cal.add('vevent')
+#cal.vevent.add('dtstamp').value = timenow
+#cal.vevent.add('dtstart').value = timenow
+#cal.vevent.add('summary').value = "At work"
+
+## Open file to (over)write. 
+## !Must fix the hard-coded path.
+#punchin = open('/home/jrsmith3/.kde/share/apps/punchclock/punchin.ics','w')
+#punchin.write(cal.serialize())
+#punchin.close()
+
+## Write some data to a simple text file that can be parsed at a later date:
+#punchbak = open('/home/jrsmith3/.kde/share/apps/punchclock/punchclock.bak','a')
+#punchbak.write("in,"+str(datetime.datetime.now())+"\n")
+#punchbak.close()
+# ========= ^^^ Old code from punch-in.py ^^^ =================================
+
+
+
+
+
+
+
 
 # In this file I need to do the following:
 # 1. Open the punch-in file and get the data as a string, then close the file.
